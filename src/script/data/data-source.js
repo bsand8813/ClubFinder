4b1a39ddc34fcd4ab61b7f759447ac922aa07a9e
Bro Sand <bsand8813@gmail.com>
2020-05-21 12:11:42 +0800

    menambahkan promise

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,14 +1,15 @@
-function DataSource(onSuccess, onFailed) {
-    this.onSuccess = onSuccess;
-    this.onFailed = onFailed;
-}
-
-DataSource.prototype.searchClub = function (keyword) {
-    const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-    if (filteredClubs.length) {
-        this.onSuccess(filteredClubs);
-    } else {
-        this.onFailed(`${keyword} is not found`);
+class DataSource {
+     
+    static searchClub(keyword) {
+        return new Promise ((resolve, reject) =>{
+            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+            console.log(filteredClubs.length);
+            if (filteredClubs.length) {
+                resolve (filteredClubs);
+            } else {
+                reject (`${keyword} is not found"`);
+            }  
+        });
+       
     }
-};
+ }
